19f989a632ef4f48844f71d9f9940d015bfb90f1
ftilde <ftilde@protonmail.com>
2019-12-14 20:50:50 +0100

    Add entries

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use rusqlite::{params, Connection};
 use std::path::Path;
 use std::str::FromStr;
 use structopt::StructOpt;
+use time::Tm;
 
 #[derive(StructOpt)]
 #[structopt()]
@@ -17,18 +18,24 @@ enum Add {
         query: String,
     },
     Other {
-        #[structopt(flatten)]
-        feed: Feed,
+        #[structopt(short = "t", long = "title", help = "Title")]
+        title: String,
+        #[structopt(short = "u", long = "url", help = "URL")]
+        url: String,
     },
+}
+
+struct Feed {
+    title: String,
+    url: String,
+    lastupdate: Option<Tm>,
 }
 
 #[derive(StructOpt)]
 #[structopt()]
-struct Feed {
-    #[structopt(short = "t", long = "title", help = "Title")]
-    title: String,
-    #[structopt(short = "u", long = "url", help = "URL")]
-    url: String,
+enum List {
+    Feeds,
+    Entries,
 }
 
 #[derive(StructOpt)]
@@ -36,7 +43,8 @@ struct Feed {
 enum Options {
     Add(Add),
     Fetch,
-    ListFeeds,
+    Refresh,
+    List(List),
 }
 
 fn youtube_url(channel: &str) -> String {
@@ -66,7 +74,7 @@ impl FeedEntries {
 struct Entry {
     title: String,
     link: String,
-    publication: String,
+    publication: Tm,
 }
 
 impl Entry {
@@ -74,14 +82,14 @@ impl Entry {
         Some(Entry {
             title: entry.title().to_owned(),
             link: entry.links().first()?.href().to_owned(),
-            publication: entry.published()?.to_owned(),
+            publication: time::strptime(entry.published()?, TIME_FORMAT_RFC3339).unwrap(),
         })
     }
     fn from_rss(entry: &rss::Item) -> Option<Self> {
         Some(Entry {
             title: entry.title()?.to_owned(),
             link: entry.link()?.to_owned(),
-            publication: entry.pub_date()?.to_owned(),
+            publication: time::strptime(entry.pub_date()?, TIME_FORMAT_RFC3339).unwrap(),
         })
     }
 }
@@ -95,12 +103,23 @@ fn parse(xml: &str) -> Result<FeedEntries, Error> {
     )))
 }
 
+const TIME_FORMAT_RFC3339: &'static str = "%Y-%m-%dT%H:%M:%S";
 const DB_NAME: &'static str = "umc.db";
-const TABLE_DEFINITION: &'static str = r#"
+const TABLE_DEFINITION_FEED: &'static str = r#"
 CREATE TABLE IF NOT EXISTS feed (
-    id      INTEGER PRIMARY KEY AUTOINCREMENT,
-    title   TEXT NOT NULL,
-    url     TEXT NOT NULL
+    feedid          INTEGER PRIMARY KEY AUTOINCREMENT,
+    title           TEXT NOT NULL,
+    url             TEXT NOT NULL,
+    lastupdate      Text
+);
+"#;
+const TABLE_DEFINITION_ENTRY: &'static str = r#"
+CREATE TABLE IF NOT EXISTS entry (
+    title          TEXT PRIMARY KEY,
+    link           TEXT NOT NULL,
+    publication    TEXT NOT NULL,
+    feedid         INTEGER,
+    FOREIGN KEY(feedid) REFERENCES feed
 );
 "#;
 
@@ -119,17 +138,46 @@ fn fetch(url: &str) -> Result<FeedEntries, Error> {
     Ok(parse(&xml_resp)?)
 }
 
-fn iter_feeds(conn: &Connection) -> Result<Vec<Result<Feed, rusqlite::Error>>, rusqlite::Error> {
+fn iter_feeds(
+    conn: &Connection,
+) -> Result<Vec<Result<(u32, Feed), rusqlite::Error>>, rusqlite::Error> {
     let mut stmt = conn.prepare(
         r#"
-        SELECT title, url FROM feed
+        SELECT feedid, title, url, lastupdate FROM feed
         "#,
     )?;
     let res = stmt
         .query_map(params!(), |row| {
-            Ok(Feed {
+            Ok((
+                row.get(0)?,
+                Feed {
+                    title: row.get(1)?,
+                    url: row.get(2)?,
+                    lastupdate: row.get(3).map(|lastupdate: Option<String>| {
+                        lastupdate.map(|lastupdate| {
+                            time::strptime(&lastupdate, TIME_FORMAT_RFC3339).unwrap()
+                        })
+                    })?,
+                },
+            ))
+        })?
+        .collect();
+    Ok(res)
+}
+
+fn iter_entries(conn: &Connection) -> Result<Vec<Result<Entry, rusqlite::Error>>, rusqlite::Error> {
+    let mut stmt = conn.prepare(
+        r#"
+        SELECT title, link, publication FROM entry
+        "#,
+    )?;
+    let res = stmt
+        .query_map(params!(), |row| {
+            let publication: String = row.get(2)?;
+            Ok(Entry {
                 title: row.get(0)?,
-                url: row.get(1)?,
+                link: row.get(1)?,
+                publication: time::strptime(&publication, TIME_FORMAT_RFC3339).unwrap(),
             })
         })?
         .collect();
@@ -144,7 +192,8 @@ fn main() -> Result<(), Error> {
     //let flags = OpenFlags::SQLITE_OPEN_FULL_MUTEX;
     //let conn = Connection::open_with_flags(db_path, flags).unwrap();
     let conn = Connection::open(db_path).unwrap();
-    conn.execute(TABLE_DEFINITION, params![])?;
+    conn.execute(TABLE_DEFINITION_FEED, params![])?;
+    conn.execute(TABLE_DEFINITION_ENTRY, params![])?;
     match Options::from_args() {
         Options::Add(add) => {
             let feed = match add {
@@ -153,13 +202,22 @@ fn main() -> Result<(), Error> {
                     Feed {
                         title: channel_name,
                         url,
+                        lastupdate: None,
                     }
                 }
                 Add::Mediathek { query } => {
                     let url = mediathek_url(&query);
-                    Feed { title: query, url }
+                    Feed {
+                        title: query,
+                        url,
+                        lastupdate: None,
+                    }
                 }
-                Add::Other { feed } => feed,
+                Add::Other { title, url } => Feed {
+                    title,
+                    url,
+                    lastupdate: None,
+                },
             };
             conn.execute(
                 r#"
@@ -170,19 +228,93 @@ fn main() -> Result<(), Error> {
         }
         Options::Fetch => {
             for feed in iter_feeds(&conn)? {
-                let feed = feed.unwrap();
+                let (_, feed) = feed.unwrap();
 
                 println!("Feed {}", feed.title);
                 let feed = fetch(&feed.url).unwrap();
                 for entry in feed.entries() {
-                    println!("\t {:?}", entry);
+                    println!(
+                        "Entry {}, url {}, publication {}",
+                        entry.title,
+                        entry.link,
+                        entry.publication.strftime(TIME_FORMAT_RFC3339).unwrap()
+                    );
                 }
             }
         }
-        Options::ListFeeds => {
+        Options::List(what) => match what {
+            List::Feeds => {
+                for feed in iter_feeds(&conn)? {
+                    let (_, feed) = feed.unwrap();
+                    println!(
+                        "Feed {}, url {}, last update: {}",
+                        feed.title,
+                        feed.url,
+                        feed.lastupdate
+                            .map(|lu| lu.strftime(TIME_FORMAT_RFC3339).unwrap().to_string())
+                            .unwrap_or("Never".to_owned())
+                    );
+                }
+            }
+            List::Entries => {
+                for entry in iter_entries(&conn)? {
+                    let entry = entry.unwrap();
+                    println!(
+                        "Entry {}, url {}, publication {}",
+                        entry.title,
+                        entry.link,
+                        entry.publication.strftime(TIME_FORMAT_RFC3339).unwrap()
+                    );
+                }
+            }
+        },
+        Options::Refresh => {
             for feed in iter_feeds(&conn)? {
-                let feed = feed.unwrap();
-                println!("Feed {}, url {}", feed.title, feed.url);
+                let (fid, feed) = feed.unwrap();
+
+                let mut lastpublication = feed.lastupdate;
+
+                for entry in fetch(&feed.url).unwrap().entries() {
+                    // FIXME: might swallow entries if entries have identical publication dates
+                    // due to < and not <=. However, <= tries to insert the latest already present
+                    // entry again.
+                    if feed.lastupdate.is_none() || feed.lastupdate.unwrap() < entry.publication {
+                        conn.execute(
+                            r#"
+                            INSERT INTO entry (title, link, feedid, publication) VALUES (?1, ?2, ?3, ?4)
+                            "#,
+                            params!(
+                                entry.title,
+                                entry.link,
+                                fid,
+                                entry
+                                    .publication
+                                    .strftime(TIME_FORMAT_RFC3339)
+                                    .unwrap()
+                                    .to_string()
+                            ),
+                        )?;
+                    }
+                    lastpublication = if let Some(lastpublication) = lastpublication {
+                        Some(entry.publication.max(lastpublication))
+                    } else {
+                        Some(entry.publication)
+                    }
+                }
+                if let Some(lastpublication) = lastpublication {
+                    conn.execute(
+                        r#"
+                        UPDATE feed SET lastupdate = ?1 WHERE feedid = ?2
+                        "#,
+                        params!(
+                            lastpublication
+                                .strftime(TIME_FORMAT_RFC3339)
+                                .unwrap()
+                                .to_string(),
+                            fid
+                        ),
+                    )?;
+                }
             }
         }
     }
